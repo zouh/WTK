c45b4a35727f0981bc31584c9e33e3a509d80bd0
zouh <zouh74@gmail.com>
2015-03-30 14:40:40 +0800

    weixin diy menu

--- app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
+++ app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
@@ -97,8 +97,8 @@ WeixinRailsMiddleware::WeixinController.class_eval do
     # 点击菜单拉取消息时的事件推送
     def handle_click_event
       #reply_text_message("你点击了: #{@keyword}")
-      reply_text_message("http://wtk.meeket.com/#{@keyword}")
-      redirect_to  "http://wtk.meeket.com/#{@keyword}"
+      reply_text_message("http://wtk8bd6.ninefold-apps.com/#{@keyword}")
+      redirect_to  "http://wtk8bd6.ninefold-apps.com/#{@keyword}"
       #redirect_to organization_diymenus_path(@organization)
     end
 
