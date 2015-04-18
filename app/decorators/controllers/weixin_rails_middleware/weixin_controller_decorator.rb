23cbf3cf880e1d776fdcda82bdb44f252cea8027
zouh <zouh74@gmail.com>
2015-04-18 08:46:01 +0800

    weixin openid

--- app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
+++ app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
@@ -84,11 +84,12 @@ WeixinRailsMiddleware::WeixinController.class_eval do
 
     # 扫描带参数二维码事件: 2. 用户已关注时的事件推送
     def handle_scan_event
+      openid = @weixin_message.FromUserName
       # weixin_client = WeixinAuthorize::Client.new(app_id, weixin_secret_key)
       # if weixin_client.is_valid?  
       #   user_info = weixin_client.user('ot_JAs5Xv1QBVOtfOSP5O2Q5CI3g')
       # end    
-      reply_text_message("#{@weixin_public_account.to_s}扫描带参数二维码事件: 2. 用户(#{@weixin_message.to_s})已关注时的事件推送, keyword: #{@keyword}")
+      reply_text_message("扫描带参数二维码事件: 2. 用户(#{openid})已关注时的事件推送, keyword: #{@keyword}")
     end
 
     def handle_location_event # 上报地理位置事件
